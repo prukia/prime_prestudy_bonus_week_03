45ebbd5b581f10c1d68524e50f2ac28f522619f5
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2016-11-27 21:21:11 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//prime prestudy bonus week 03
+function addIt(x){
+	return x+2;
+}
+function multiplyIt(x){
+	return x*3;
+}
+function subtractIt(x){
+	return x-4;
+}
+console.log(addIt(multiplyIt(subtractIt(x))))
